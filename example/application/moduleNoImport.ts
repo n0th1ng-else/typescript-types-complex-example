919755c02061af530e32a5898a7ed08b978a58ee
Sergey Nikitin <sen@tradeshift.com>
2020-12-12 00:07:25 +0700

    fix(): Fixes

--- example/application/moduleNoImport.ts
+++ example/application/moduleNoImport.ts
@@ -3,3 +3,9 @@
 ui.notification.info("Document has been saved!");
 
 ui.notification.warning("Sounds scary", [{ text: "Yes" }]);
+
+const showNotificationWithButton = (
+  buttonText: string,
+  buttonType: UiCore.ButtonType
+): void =>
+  ui.notification.info("test", [{ text: buttonText, type: buttonType }]);
